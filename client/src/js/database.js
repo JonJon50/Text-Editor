49fb31e19b5b99954a751034f5fe91ae66cd42e9
John Hagens <ginuwine104@gmail.com>
2023-02-27 17:02:59 -0500

    database: get/delete

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,35 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  // Open the database
+  const db = await initdb();
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Add the content to the 'jate' object store
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  await store.add(content);
+
+  // Wait for the transaction to complete
+  await tx.complete;
+
+  // Log that the content has been added successfully
+  console.log('Content added to jate object store');
+};
+
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  // Open the database
+  const db = await initdb();
+
+  // Get all the content from the 'jate' object store
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const allContent = store.getAll();
+
+  // Return a promise that resolves to the content
+  return allContent;
+};
 
 initdb();
